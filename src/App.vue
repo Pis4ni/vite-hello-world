f4c68e1082ee3d29d676195c75443412869dc020
Pis4ni <pis4wrk@gmail.com>
2023-09-07 17:18:04 +0200

    add: milestone 0

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import AppTitle from "./components/AppTitle.vue";
+export default {
+
+  data() {
+    return {
+      message:'realizzato con un template vite/vue'
+    }
+  },
+  components: {
+    AppTitle,
+    // AppJumbo
+  }
+  
+}
+</script>
+
+<template>
+  <AppTitle></AppTitle>
+  <hr>
+  <span>{{ message }}</span>
+  <hr>
+  <!-- <AppJumbo></AppJumbo> -->
+
+  
+</template>
+<style>
+ 
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+
+export default {
+    data() {
+        return {
+            title:'Vue Hello World'
+        }
+    }
+}
+</script>
+
+<template>
+      <h1> {{ title }} </h1> 
+</template>
+
+<style scoped>
+
+</style>
